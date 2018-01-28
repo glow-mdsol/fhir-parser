afb07faef2d773d7dd0a86ff5e39ada29a02ce9a
Geoff Low <glow@mdsol.com>
2018-01-28 12:50:14 -0600

    change the template to use Enum add the URL property to hold the system.url and make it accessible

--- Sample/template-codesystems.py
+++ Sample/template-codesystems.py
@@ -8,9 +8,10 @@
 #  THIS HAS BEEN ADAPTED FROM Swift Enums WITHOUT EVER BEING IMPLEMENTED IN
 #  Python, FOR DEMONSTRATION PURPOSES ONLY.
 #
+from enum import Enum
 {% for system in systems %}{% if system.generate_enum %}
 
-class {{ system.name }}(object) {
+class {{ system.name }}(Enum):
 	""" {{ system.definition.description|wordwrap(width=120, wrapstring="\n") }}
 
 	URL: {{ system.url }}
@@ -18,11 +19,11 @@ class {{ system.name }}(object) {
 	ValueSet: {{ system.definition.valueSet }}
 	{%- endif %}
 	"""
+	URL = "{{ system.url }}"
 	{%- for code in system.codes %}
-	
+
 	{{ code.name }} = "{{ code.code }}"
 	""" {{ code.definition|wordwrap(width=112, wrapstring="\n	/// ") }}
 	"""
 	{%- endfor %}
-}
 {% endif %}{% endfor %}
