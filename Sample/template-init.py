1c667ec9d54063db9315ab937975eb998867c7d3
Geoff Low <glow@mdsol.com>
2018-05-09 11:20:08 +0100

    Added the __init__.py template

---
+++ Sample/template-init.py
@@ -0,0 +1,11 @@
+#
+#  __init__.py
+#  client-py
+#
+#  Generated from FHIR {{ info.version }} on {{ info.date }}.
+#  {{ info.year }}, SMART Health IT.
+#
+
+FHIR_VERSION = '{{ info.version }}'
+GENERATION_DATE = '{{ info.date }}'
+
