1d86c416dba0d28bd51327cbce631d92b8666314
Geoff Low <glow@mdsol.com>
2018-05-09 12:49:02 +0100

    reformatted template

--- Sample/template-codesystems.py
+++ Sample/template-codesystems.py
@@ -14,6 +14,7 @@ class CodeSystemValue(Enum):
     """
     Base CodeSystemValue class
     """
+
     @property
     def as_coding_dict(self):
         return dict(display=self.value, system=self.URL, code=self.name)
@@ -22,27 +23,28 @@ class CodeSystemValue(Enum):
     def text(self):
         return self.value
 
+
 {%- for system in systems %}
 {%- if system.generate_enum %}
 
 
-class {{ system.name }}(CodeSystemValue):
-	""" {{ system.definition.description|wordwrap(width=120, wrapstring="\n") }}
+class {{system.name}}(CodeSystemValue):
+    """ {{ system.definition.description|wordwrap(width=120, wrapstring="\n") }}
 
-	URL: {{ system.url }}
-	{%- if system.definition.valueSet %}
-	ValueSet: {{ system.definition.valueSet }}
-	{%- endif %}
-	"""
-	URL = "{{ system.url }}"
-    {%- if system.definition.experimental is defined %}}
+    URL: {{ system.url }}
+    {%- if system.definition.valueSet %}
+    ValueSet: {{ system.definition.valueSet }}
+    {%- endif %}
+    """
+    URL = "{{ system.url }}"
+    {%- if system.definition.experimental is defined %}
     EXPERIMENTAL = {{system.definition.experimental}}
     {%- endif %}
-	{%- for code in system.codes %}
+    {%- for code in system.codes %}
 
-	""" {{ code.definition|wordwrap(width=112, wrapstring="\n	/// ") }}
-	"""
-	{{ code.name }} = "{{ code.code }}"
-	{%- endfor %}
+    """ {{ code.definition|wordwrap(width=112, wrapstring="\n	/// ") }}
+    """
+    {{code.name}} = "{{ code.code }}"
+    {%- endfor %}
 {%- endif %}
 {%- endfor %}
